84a247190d1f65d8041f2d2d6d839244e1ba07e3
d1nsol <jinsik0126@gmail.com>
2020-10-27 17:36:42 +0900

    config modify

--- src/components/Config.js
+++ src/components/Config.js
@@ -1,4 +1,5 @@
 //SERVER ROUTES
 export const USER_SERVER = 'https://vast-cliffs-99081.herokuapp.com/api/users';
+//export const USER_SERVER = 'http://localhost:5000/api/users';
 export const INSURANCE_SERVER = 'https://stark-island-15004.herokuapp.com/api/insurance';
 //export const INSURANCE_SERVER = 'http://localhost:5000/api/insurance';
